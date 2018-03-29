8bc67cac9e1cfdf2c6ec65eb2cb6a71345b49181
Konstantin Vakula <v_kostya94@mail.ru>
2018-03-29 23:51:52 +0700

    [Lesson1] Edit my first programm

---
+++ Example.java
@@ -0,0 +1,11 @@
+package com.example1;
+
+public class Example {
+
+    /**
+     * Это пример простой программы на Java.
+     */
+    public static void main(String[] args) {
+        System.out.println("Java правит миром!!!");
+    }
+}
